f11fd94448083f4ca46a23d23911f1abf55c3bad
johnoneil <oneil.john@gmail.com>
2016-11-20 12:41:25 -0800

    Improved emulated bytecode.

--- main.cpp
+++ main.cpp
@@ -12,28 +12,129 @@ dynrec experiment for performance improvment measure
 
 #if __EMSCRIPTEN__
 #include <emscripten.h>
+
+#define __DYNREC__ 0
+
 #endif
 
 
-#define LOOPS 100000
+#define LOOPS 3
 
 class VirtualMachine {
 public:
-  //enum OPCODES {
-  //  ADD_I = 0,
-  // }
 
-  //int ic;
+
+#if __EMSCRIPTEN__
+  void dynrec_startMethod(const char* name, const int argc)
+  {
+    std::cout<<__func__<<std::endl;
+
+    EM_ASM_INT({
+        var name = Pointer_stringify($0|0);
+
+        Module.dynrec = Module.dynrec || {};
+        Module.dynrec.methods = Module.dynrec.methods || {};
+        Module.dynrec.currentMethod = name;
+        code += 'var locals = [];\n';
+        code += 'var r0 = 0;\n';
+        Module.dynrec.code = code; 
+
+        return 0;
+    }
+    ,name);
+  };
+  void dynrec_endMethod()
+  {
+    std::cout<<__func__<<std::endl;
+    EM_ASM({
+      
+      var name = Module.dynrec.currentMethod;
+      
+      var code = Module.dynrec.code;
+      code += 'return r0;\n'
+      
+      console.log(code);
+      console.error('DYNREC method: ', name,' code-->',code);
+
+      //Module.dynrec.methods[name] = new Function("a1", "a2", Module.dynrec.code);
+    });
+  };
+  int dynrec_callmethod_i(const char* name, const int a, const int b)
+  {
+    return EM_ASM_INT({
+      var name = Pointer_stringify($0|0);
+      var a = $1;
+      var b = $2;
+      return Module.dynrec.methods[name](a,b);
+    }
+    ,name
+    ,a
+    ,b);
+  };
+
+  void emit_push_i()
+  {
+    EM_ASM({
+      Module.dynrec.code += 'locals.push(;\n';
+    });
+  }
+  void emit_add_i()
+  {
+    EM_ASM({
+      Module.dynrec.code += '{\n';
+      Module.dynrec.code += 'var l0 = locals[l-1] + locals[l]\n;'
+      Module.dynrec.code += 'locals.pop();\nlocals.pop();\n';
+      Module.dynrec.code += 'locals.push(l0);\n';
+      Module.dynrec.code += '}\n';
+    });
+  }
+  void emit_pop_i()
+  {
+    EM_ASM({
+      Module.dynrec.code += 'locals[l] = arguments.pop();\n'
+    });
+  }
+  void emit_return_i()
+  {
+    EM_ASM({
+      Module.dynrec.code += 'var r0 = locals[l]; l--;return r0;';
+    });
+  }
+#endif
+
   std::stack<int> s;
+  int locals[100];
+
+  void setlocal_i(const int reg)
+  {
+    locals[reg] = s.top();
+    s.pop();
+  }
+
+  void getlocal_i(const int reg)
+  {
+    s.push(locals[reg]);
+  }
+
+  int return_local_i(const int reg)
+  {
+    return locals[reg];
+  }
 
   void push_i(int v) {
     s.push(v);
+#if __EMSCRIPTEN__ && __DYNREC__
+    emit_push_i();
+#endif
   };
 
   int pop_i()
   {
     int r = s.top();
     s.pop();
+#if __EMSCRIPTEN__ && __DYNREC__
+    emit_pop_i();
+#endif
     return r;
   }
 
@@ -41,9 +142,21 @@ public:
   {
     int a = pop_i();
     int b = pop_i();
+
+#if __EMSCRIPTEN__ && __DYNREC__
+    emit_add_i();
+#endif
+
     s.push(a+b);
   }
+
 };
+
+#if __EMSCRIPTEN__
+
+void dummy(){};
+
+#endif
 
 
 int main(int argc, char* argv[])
@@ -53,20 +166,41 @@ int main(int argc, char* argv[])
   int r = 0;
 
   VirtualMachine vm;
-  
+ 
+#if __EMSCRIPTEN__ && __DYNREC__
+  vm.dynrec_startMethod("sum");
+#endif
+ 
   using namespace std::chrono;
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
   // Let's say we have a function that has a long series of number
   // manipulating ocodes. Every time we call this method we traverse these
   // opcodes and do work for each one.
+
+  vm.push_i(a);
+  vm.push_i(b);
+
   for(int i=0; i<LOOPS; ++i)
   {
-    vm.push_i(a);
-    vm.push_i(b);
+
+    // method had two arguments on stack: a0, a1
+    // method returns one value r0;
+
+    vm.setlocal_i(2);
+    vm.setlocal_i(1);
+    vm.getlocal_i(1);
+    vm.getlocal_i(2);
     vm.add_i();
-    a = vm.pop_i();
+    vm.setlocal_i(1);
+    vm.getlocal_i(1);
+    vm.getlocal_i(2);
   }
+  a = vm.return_local_i(1);
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+#if __EMSCRIPTEN__ && __DYNREC__
+  vm.dynrec_endMethod();
+#endif
 
   std::cout<<"Virtual machine implementaition result: "<<a<<std::endl;
   duration<double> time_span = duration_cast<duration<double>>(t2-t1);
@@ -81,6 +215,13 @@ int main(int argc, char* argv[])
   b = 1;
   r = 0;
   int loops =  LOOPS;
+
+#if 0 //__DYNREC__
+
+  r = vm.dynrec_callmethod_i("sum", a, b);
+
+#else // __DYNREC__
+
   high_resolution_clock::time_point t3 = high_resolution_clock::now();
   
   r = EM_ASM_INT({
@@ -104,6 +245,8 @@ int main(int argc, char* argv[])
   ,b
   ,loops);
 
+#endif // __DYNREC__
+
   high_resolution_clock::time_point t4 = high_resolution_clock::now();
 
   std::cout<<"Virtual machine implementaition result: "<<r<<std::endl;
@@ -113,6 +256,10 @@ int main(int argc, char* argv[])
 
 #endif // __EMSCRIPTEN__
 
+
+#if __EMSCRIPTEN__
+  emscripten_set_main_loop(dummy, 0, 1);
+#endif
 
   return 0;
 
